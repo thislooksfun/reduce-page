d34750188928febce88893a05e45a1eb85f43dc0
thislooksfun <tlf@thislooks.fun>
2023-06-01 00:16:06 -0500

    style: enable no-console eslint rule

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable no-console */
 
 import type { ReductionStep } from "./types.js";
 import type { FormatInputPathObject } from "node:path";
